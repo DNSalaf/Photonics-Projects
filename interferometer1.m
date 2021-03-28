022dee055e97565bedc3a809adae2fafadee9054
gustavo-r-martins <57540458+gustavo-r-martins@users.noreply.github.com>
2021-03-28 14:24:20 -0300

    Update interferometer1.m

--- interferometer1.m
+++ interferometer1.m
@@ -12,17 +12,46 @@ T = 1/freq;
 dist = 2e-2;
 dz = 0.1e-3;
 z = [dist dist+dz];
-t = linspace(0,T,len);
+t = linspace(0,1.5*T,len);
 
 wave1 = exp(-1j*( k*z(1) - w.*t ));
 wave2 = exp(-1j*( k*z(2) - w.*t ));
 
 figure(1)
+    sgtitle('Interferência');
+subplot(2,2,1)
+    plot(t/T,real(wave1),'--'); hold on;
+    plot(t/T,real(wave2),'--');
+    plot(t/T,real(wave1 + wave2),'k' );
+    plot(t/T,zeros(1,len),'k');
+    ylabel('Amplitude (u.a.)');
+
+z = [dist dist+3*dz];
+wave1 = exp(-1j*( k*z(1) - w.*t ));
+wave2 = exp(-1j*( k*z(2) - w.*t ));
+subplot(2,2,2)
     plot(t/T,real(wave1)); hold on;
-    plot(t/T,real(wave2));
-    plot(t/T,real(wave1 + wave2) );
+    plot(t/T,real(wave2),'--');
+    plot(t/T,real(wave1 + wave2),'k' );
+    plot(t/T,zeros(1,len),'k');
+    
+z = [dist dist+0.006*dz];
+wave1 = exp(-1j*( k*z(1) - w.*t ));
+wave2 = exp(-1j*( k*z(2) - w.*t ));
+subplot(2,2,3)
+plot(t/T,real(wave1),'--'); hold on;
+    plot(t/T,real(wave2),'--');
+    plot(t/T,real(wave1 + wave2),'k' );
     plot(t/T,zeros(1,len),'k');
     xlabel('t/T');
     ylabel('Amplitude (u.a.)');
-    legend('Short Path','Long Path','Interferência');
-    title('Interferômetro Mach-Zehnder');
+    
+z = [dist dist+0.0075*dz];
+wave1 = exp(-1j*( k*z(1) - w.*t ));
+wave2 = exp(-1j*( k*z(2) - w.*t ));
+subplot(2,2,4)
+plot(t/T,real(wave1),'--'); hold on;
+    plot(t/T,real(wave2),'--');
+    plot(t/T,real(wave1 + wave2),'k' );
+    plot(t/T,zeros(1,len),'k');
+    xlabel('t/T');
